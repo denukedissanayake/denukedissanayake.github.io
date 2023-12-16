2cf5f2ec46ce2010d644928bea0c643074eec6f4
Denuke <ddnadhananja@gmail.com>
2023-12-16 22:58:18 +0530

    Add header

---
+++ src/app/components/header.tsx
@@ -0,0 +1,9 @@
+import './header.css'
+
+export const Header = () => (
+    <div className="header">
+        <div className="navigation">
+            <a href="#">CHECK MY BLOG</a>
+        </div>
+    </div>
+)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import { Header } from './components/header'
+
 export default function Home() {
   return (
-    <h1>Hello!</h1>
+    <><Header/></>
   )
 }
