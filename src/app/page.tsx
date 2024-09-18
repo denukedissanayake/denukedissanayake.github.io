ed76b17d1ebe41ad1484194079c9fc63ca573b51
denukedissanayake <ddnadhananja@gmail.com>
2024-09-19 03:40:28 +0530

    intiate the project

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+import Home from "@/components/sections/Home";
+
+export default function Page() {
+  return (
+    <div>
+      <Header/>
+      <Home/>
+    </div>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <div className="">
+            <div className="">
+                <a href="#"><span className="">Header</span></a>
+            </div>
+        </div>
+    )
+}
