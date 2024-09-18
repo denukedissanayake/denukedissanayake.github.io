8000578fb37fcdc04c12a1ade846bb4cd950026e
denukedissanayake <ddnadhananja@gmail.com>
2024-09-19 04:13:15 +0530

    add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 export default function Header() {
     return (
-        <div className="">
-            <div className="">
-                <a href="#"><span className="">Header</span></a>
+        <div className="w-screen flex justify-end p-2">
+            <div className="pr-5">
+                <a href="#"><span className="font-bold">CHECK MY BLOG</span></a>
             </div>
         </div>
     )
